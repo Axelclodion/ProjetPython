db8892fc8c84bf5ce5dd4e0dbd3bcc2b127c68fe
Axel CLODION <71276286+Axelclodion@users.noreply.github.com>
2023-03-27 04:27:13 +0200

    Update 1.generer.py

--- 1.generer.py
+++ 1.generer.py
@@ -7,14 +7,21 @@ url = "https://bsbstore.fr/collections/vetements-hf"
 
 # Envoi de la requête HTTP au site web et récupération du contenu HTML
 response = requests.get(url)
-content = response.content
+if response.status_code == 200:
+    content = response.content
+else:
+    print("Erreur de requête : {}".format(response.status_code))
+    content = None
 
 # Analyse du contenu HTML à l'aide de BeautifulSoup
-soup = BeautifulSoup(content, 'html.parser')
+if content is not None:
+    soup = BeautifulSoup(content, 'html.parser')
 
-# Récupération de toutes les balises 'a' contenant des liens
-links = soup.find_all('a')
+    # Récupération de toutes les balises 'a' contenant des liens
+    links = soup.find_all('a')
 
-# Parcours des liens et affichage de leur contenu
-for link in links:
-    print(link.text)
+    # Parcours des liens et affichage de leur contenu
+    for link in links:
+        print(link.text)
+else:
+    print("Impossible de récupérer le contenu HTML.")
