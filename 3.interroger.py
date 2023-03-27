b67bd82ea3c94f18b4977251592a339ce35467db
Axel CLODION <71276286+Axelclodion@users.noreply.github.com>
2023-03-27 04:31:50 +0200

    Update 3.interroger.py

--- 3.interroger.py
+++ 3.interroger.py
@@ -1,17 +1,19 @@
 
 import streamlit as st
 import pandas as pd
+import sqlite3
 
+Connexion à la base de données SQLite
+conn = sqlite3.connect('example.db')
 
-# Connexion à la base de données MySQL
-cnx = mysql.connector.connect(user='username', password='password', host='localhost', database='database')
+Exécution d'une requête SQL pour récupérer les données de la table 'products'
+df = pd.read_sql_query("SELECT * FROM products", conn)
 
-# Exécution d'une requête SQL pour récupérer les données de la table 'table_name'
-query = "SELECT * FROM table_name"
-df = pd.read_sql(query, cnx)
-
-# Affichage des données sous forme de graphique
+Affichage des données sous forme de graphique
 st.line_chart(df)
 
-# Affichage des données sous forme de tableau
+Affichage des données sous forme de tableau
 st.write(df)
+
+Fermeture de la connexion
+conn.close()
